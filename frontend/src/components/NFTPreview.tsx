7c7a841229062a067b24b94f78ba142eef3c214b
Michael Mueller <hello@michaelmueller.dev>
2021-10-23 18:47:31 +0100

    chore: wire up API

--- frontend/src/components/NFTPreview.tsx
+++ frontend/src/components/NFTPreview.tsx
@@ -1,7 +1,22 @@
-
+import { useEffect, useState } from 'react';
+import { getNFTsByOwner } from '../api';
 
 const NFTPreview = () => {
-    return <div>NFTPreview</div>
-}
+  const [nfts, setNfts] = useState({});
+
+  useEffect(() => {
+    const fetchNFTs = async () => {
+      const {
+        data: {
+          data: { owner },
+        },
+      } = await getNFTsByOwner('0x7be8076f4ea4a4ad08075c2508e481d6c946d12b');
+      setNfts(owner);
+    };
+    fetchNFTs();
+  }, []);
+
+  return <code>NFTPreview {JSON.stringify(nfts, null, 2)}</code>;
+};
 
 export default NFTPreview;
