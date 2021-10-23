d1d7174b0b2734e05ac47074631350d18dd7aa41
Konrad Gnat <konradmgnat@gmail.com>
2021-10-23 16:01:19 +0100

    add swap panel component

--- frontend/src/components/SwapPanel.tsx
+++ frontend/src/components/SwapPanel.tsx
@@ -1,5 +1,22 @@
+import './SwapPanel.css';
+import swap from '../images/swap.png';
+
 const SwapPanel = () => {
-    return <div>swap panel</div>
+    return (
+        <div className="swappanel">
+            <div className="swappanel__sub">
+                <span className="swappanel__label">From</span>
+                <h3>Ethereum</h3>
+            </div>
+            <div className="swappanel__switch">
+                <img src={swap} alt="" className="swappanel__img" />
+            </div>
+            <div className="swappanel__sub">
+                <span className="swappanel__label">To</span>
+                <h3>Avalanche</h3>
+            </div>
+        </div>
+    );
 }
 
 export default SwapPanel;
