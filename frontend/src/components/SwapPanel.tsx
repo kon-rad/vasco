f511fb58fb93a2b7adb8c98ef30fbbf2fb707587
Konrad Gnat <konradmgnat@gmail.com>
2021-10-23 22:55:22 +0100

    add chain icons

--- frontend/src/components/SwapPanel.tsx
+++ frontend/src/components/SwapPanel.tsx
@@ -3,6 +3,8 @@ import './SwapPanel.css';
 import { useWeb3React } from '@web3-react/core';
 import { useSupportedChain } from '../contexts/supportedChain';
 import swap from '../images/swap.png';
+import EthLogo from '../images/eth_logo.png';
+import AvaxLogo from '../images/avax_logo.png';
 
 const SwapPanel = () => {
   const [direction, setDirection] = useState<number>(0);
@@ -15,11 +17,17 @@ const SwapPanel = () => {
     deactivate();
   };
   const renderDirection = ['Ethereum', 'Avalanche'];
+  const renderIcon = (direction: number) => {
+      return direction === 0 ? <img src={EthLogo} className="swappanel__logo" /> : <img src={AvaxLogo} className="swappanel__logo" />;
+  }
   return (
     <div className='swappanel'>
       <div className='swappanel__sub'>
         <span className='swappanel__label'>From</span>
-        <h3>{renderDirection[direction]}</h3>
+        <div className="swappanel__direction">
+            {renderIcon(direction)}
+            <h3>{renderDirection[direction]}</h3>
+        </div>
       </div>
       <div className='swappanel__switch'>
         <img
@@ -31,7 +39,10 @@ const SwapPanel = () => {
       </div>
       <div className='swappanel__sub'>
         <span className='swappanel__label'>To</span>
-        <h3>{renderDirection[direction === 0 ? 1 : 0]}</h3>
+        <div className="swappanel__direction">
+            {renderIcon(direction === 0 ? 1 : 0)}
+            <h3>{renderDirection[direction === 0 ? 1 : 0]}</h3>
+        </div>
       </div>
     </div>
   );
